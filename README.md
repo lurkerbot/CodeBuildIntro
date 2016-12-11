38c426a20cebb494ee954253c364891692229d8a
Marten Payne <martenp@amazon.com>
2016-12-12 10:15:56 +1100

    Added skeletal server main and updated README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CodeBuildIntro
+
+Simple port 8080 server to allow demonstration of minimal CodeBuild/CodePipeline
+CI deployment
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    // fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "Greetings CodeDeploy :)")
+}
+
+/**
+ * Simple port 8080 server to allow demonstration of minimal CodeBuild/CodePipeline
+ * CI deployment
+ */
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
