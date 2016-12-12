88749091c76a6b96ffaea617775582de525d968c
Marten Payne <martenp@amazon.com>
2016-12-12 23:09:21 +1100

    change default port

--- main.go
+++ main.go
@@ -16,5 +16,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
  */
 func main() {
     http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe(":1234", nil)
 }
